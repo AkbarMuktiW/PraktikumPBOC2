d36d25c3e8ed5faac5be5361e903314526a04acd
Akbar Mukti Wibowo <akbarmuktiwibowo23@gmail.com>
2026-03-13 00:30:01 +0700

    Lanjutan progress lab

--- Pertemuan4/Pertemuan4/src/BangunDatar.java
+++ Pertemuan4/Pertemuan4/src/BangunDatar.java
@@ -1,53 +1,66 @@
+/* Nama File : BangunDatar.java
+ * Deskripsi : berisi attribut dan method dalam class BangunDatar
+ * Pembuat : Akbar Mukti Wibowo 
+ * Tanggal : 13 Maret 2026
+ */
+
 public class BangunDatar {
     // =============ATRIBUT================
-  private int jmlSisi;
-  private String warna;
-  private String border;
-  private static int counterBangunDatar = 0;
+    private int jmlSisi;
+    private String warna;
+    private String border;
+    private static int counterBangunDatar = 0;
 
-  //=============KONSTRUKTOR=============
-  public BangunDatar() {
-  }
+    //=============KONSTRUKTOR=============
+    // tanpa parameter
+    public BangunDatar() {
+        counterBangunDatar++;
+    }
 
-  public BangunDatar(int jmlSisi, String warna, String border) {
-    this.jmlSisi = jmlSisi;
-    this.warna = warna;
-    this.border = border;
-  }
+    // dengan parameter
+    public BangunDatar(int jmlSisi, String warna, String border) {
+        this.jmlSisi = jmlSisi;
+        this.warna = warna;
+        this.border = border;
+        counterBangunDatar++;
+    }
 
-  public static void printCounterBangunDatar() {
-    System.out.println("Jumlah objek bangun datar : " + counterBangunDatar);
-  }
+    
+    //=============GETTER SETTER=================
+    public int getJmlSisi() {
+        return jmlSisi;
+    }
+    
+    public void setJmlSisi(int jmlSisi) {
+        this.jmlSisi = jmlSisi;
+    }
+    
+    public String getWarna() {
+        return warna;
+    }
+    
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    
+    public String getBorder() {
+        return border;
+    }
+    
+    public void setBorder(String border) {
+        this.border = border;
+    }
+    
+    //=============METHOD===============
+    // Mencetak informasi bangun datar
+    public void printInfo() {
+        System.out.println("Jumlah Sisi : " + jmlSisi);
+        System.out.println("Warna : " + warna);
+        System.out.println("Border : " + border);
+    }
 
-  //=============GETTER SETTER=================
-  public int getJmlSisi() {
-    return jmlSisi;
-  }
-
-  public void setJmlSisi(int jmlSisi) {
-    this.jmlSisi = jmlSisi;
-  }
-
-  public String getWarna() {
-    return warna;
-  }
-
-  public void setWarna(String warna) {
-    this.warna = warna;
-  }
-
-  public String getBorder() {
-    return border;
-  }
-
-  public void setBorder(String border) {
-    this.border = border;
-  }
-
-  public void printInfo() {
-    System.out.println("Jumlah Sisi : " + jmlSisi);
-    System.out.println("Warna : " + warna);
-    System.out.println("Border : " + border);
-  }
-
+    // Mengeluarkan jumlah bangun datar yang dibuat
+    public static void printCounterBangunDatar() {
+        System.out.println("Jumlah objek bangun datar : " + counterBangunDatar);
+    }
 }
